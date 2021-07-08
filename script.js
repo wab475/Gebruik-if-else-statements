f8ef712769000bfdb242a08ca5edd70e9fb2ee11
wab475 <84964630+wab475@users.noreply.github.com>
2021-07-08 20:39:50 +0200

    Opdracht_014 Gebruik if/else statements

---
+++ script.js
@@ -0,0 +1,21 @@
+var myAge = 23;
+
+if (myAge >= 18) {
+    console.log("je mag naar binnen, veel plezier")
+} else {
+    console.log("Je bent nog geen 18, je mag helaas niet naar binnen")
+}
+
+var isFemale = false;
+if (isFemale) {
+    console.log("Welkom op Ladiesnight!")
+} else {
+    console.log("Helaas, vanavond geen feestje voor de mannen!")
+}
+
+let driverStatus = "Nope";
+if (driverStatus == "Bob") {
+    console.log("Top Bob, veilig naar huis")
+} else {
+    console.log("Zoek een Bob om je naar huis te brengen!")
+}
